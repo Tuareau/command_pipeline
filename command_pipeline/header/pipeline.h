0a19ae2a0c39a4244ee48b03bc2fa80560dadb43
Pavel Matviyuk <pav.matt@mail.ru>
2021-11-11 15:50:51 +0300

    Add Pipeline class realization
    Pipeline includes array of commands,
    where each index refers to the stage
    the command goes through. It can be five
    stages. Also it keeps vector of commands
    to execute that will be filled in
    constructor.

---
+++ command_pipeline/header/pipeline.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#ifndef _PIPELINE_H_
+#define _PIPELINE_H_
+
+#include <array>
+#include <vector>
+
+#include "command.h"
+#include "generator.h"
+
+class StatisticsCollector;
+
+class Pipeline
+{
+public:
+	enum class Stage {
+		DECODE,
+		FETCH_LEFT_OPERAND,
+		FETCH_RIGHT_OPERAND,
+		EXECUTE,
+		WRITE_BACK,
+	};
+	const static size_t STAGES_COUNT = 5;
+	size_t stage_to_index(const Stage stage) const;
+
+private:
+	StatisticsCollector _stats_collector;
+
+	std::array<Command *, STAGES_COUNT> _executing_commands;
+	std::vector<Command> _commands_vector;
+
+public:
+	explicit Pipeline(size_t commands_count);
+	~Pipeline() = default;
+
+	void run();
+};
+
+#endif
